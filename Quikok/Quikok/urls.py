98f314f70f3db9b07abb00c6e870a1c1722a873a
Annie <colorfulday0123@gmail.com>
2020-07-08 17:41:34 +0800

    套用bootstrap的主題模板到base、新增base_layout，並設定當中的static連結

--- Quikok/Quikok/urls.py
+++ Quikok/Quikok/urls.py
@@ -7,6 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', views.homepage),
+    path('base_layout/', views.base_layout),
     path('account/', include('account.urls')),
 ]
 
--- Quikok/Quikok/views.py
+++ Quikok/Quikok/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib import admin
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def base_layout(request):
+    return render(request, 'base_layout.html')
