3c6c29d37ddba0924521210a50590f9b36973a77
tata <tamio.chou@gmail.com>
2020-07-09 13:07:32 +0800

    新增dev_.html與function，用來單元檢測開發中的頁面每個功能是否運作正常。

--- Quikok/account/urls.py
+++ Quikok/account/urls.py
@@ -3,5 +3,6 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('user_signup/', views.signup_function)
+    path('user_signup/', views.signup_function),
+    path('dev_user_signup/', views.test_func)
 ]
