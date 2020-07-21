725995e98e5ea1a8995adfbd9b6b8d3af96058db
Annie <colorfulday0123@gmail.com>
2020-07-21 10:16:38 +0800

    課程頁面的呈現方式：用col-12包起來，每個col裡有很多buttom

--- Quikok/lesson/views.py
+++ Quikok/lesson/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def lessons_main_page(request):
-    #title = '開課! Quikok - 課程主頁'
-    return render(request, 'lesson/lessons_main_page.html')
+    title = '開課! Quikok - 課程主頁'
+    main_subjucts_list = ['國文','英文','數學']
+    #if request.method == 'POST':
+    #    subjects = request.POST.get('subjects')
+    #    print(subjects)
+
+    if 'subjects' in request.POST:
+        if request.POST['subjects'] is not '':
+            
+            print(request.POST['platform'])
+
+
+
+    return render(request, 'lesson/lessons_main_page.html', locals())
