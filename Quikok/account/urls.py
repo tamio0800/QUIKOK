ebdb3cc24f35dd99797039fddd3a4574e15e522f
Annie <colorfulday0123@gmail.com>
2020-07-31 10:09:37 +0800

    update

--- Quikok/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from .import views
-from django.urls import path
-
-urlpatterns = [
-    path('user_signup/', views.signup),
-    path('dev_user_signup/', views.dev_signup)
-]
