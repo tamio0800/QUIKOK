c38478b0ec0951585ca74aa9252131f8a6f3bbe6
Hsinyuan <hsinyuan@cycu.org.tw>
2020-08-05 20:53:10 -0700

    test success

--- Quikok/chatroom/tests.py
+++ Quikok/chatroom/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
 
+from Quikok.chatroom.models import Messages, chat_room
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+if __name__ == '__main__':
+
+  
+    user1=User.objects.get(id=user1id)
+    user2=User.objects.get(id=user2id)
+    chat_room.objects.create(member1=user1,member2=user2)
