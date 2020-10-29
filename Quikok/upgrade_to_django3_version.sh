1476ae0ad8429865f6e4b50dac803c9cf613def8
tamio <tamio.chou@gmail.com>
2020-10-29 13:49:57 +0800

    Django升級至3.1.2版本 操作方式: 1. sudo apt update -y 2. sudo apt upgrade -y 3. conda deactivate 4. conda env remove -n quikok                  << 這個是為了重建虛擬環境 5. conda create -n quikok python=3.7 -y 6. conda activate quikok 7. pip install -r requirements_upgraded.txt 8. 執行python manage.py runserver時若有raise ImproperlyConfigured('mysqlclient 1.4.0 or newer is required; you have %s.' % Database.__version__)錯誤，則到上方的base.py，將35、36行註釋掉即可正常運作。

--- Quikok/upgrade_to_django3_version.sh
+++ Quikok/upgrade_to_django3_version.sh
@@ -1,1 +1,1 @@
-sudo apt update -y && sudo apt upgrade -y && conda deactivate && conda env remove --name quikok -y && conda create -n quikok python=3.7 -y && pip install -r requirements_upgraded.txt
+sudo apt update -y && sudo apt upgrade -y && conda deactivate && conda env remove -n quikok && conda create -n quikok python=3.7 -y && pip install -r requirements_upgraded.txt
