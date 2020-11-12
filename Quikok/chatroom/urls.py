7dcbd0a1d0f15c63dfebbf84601941537c417fdc
annie <colorfulday0123@gmail.com>
2020-11-12 18:49:32 +0800

    改聊天室url

--- Quikok/chatroom/urls.py
+++ Quikok/chatroom/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('check_or_create_chatroom/',views.check_if_chatroom_exist),
+    path('check_or_create_chatroom/',views.checkOrCreateChatroom),
     path('<str:user_url>/', views.chat, name='chat'),
 ]
