751fd7b691f26cba0cfcf8c27b36506c11c03be6
Annie <colorfulday0123@gmail.com>
2020-11-17 18:59:11 +0800

    聊天室回傳歷史訊息更新:主架構完成小蟲修正中

--- Quikok/chatroom/chat_tools.py
+++ Quikok/chatroom/chat_tools.py
@@ -14,7 +14,7 @@ class chat_room_manager:
         # 依據要回傳給前端的字典格式建立空的self字典
         # response_msg字典裏面又包含兩層字典
         # 因為感覺之後會直接叫裡面的字典來修改、再更新較外層的字典,
-        # 因此先將這幾層字典都設 self (尚不清楚是否可以這樣做XD)
+        # 因此先將這幾層字典都設 self 
         response_msg_key = ['chatroomID','chatUnreadMessageQty', 'chatUserID', 'chatUserType',
                         'chatUserName', 'chatUserPath', 'messageInfo']
         self.response_msg = dict()
@@ -25,12 +25,14 @@ class chat_room_manager:
         self.message_info =dict()
         for key in a_message_info_dict_keys:
             self.message_info[key] = None  
+        # 預約message 的keys的keys
         booking_related_message_key = ['bookingID', 'bookingLeesonID', 'bookingStatus',
-                                        'bookingDate', 'bookingTime', 'bookingUpdateTime']
+                                      'bookingDate', 'bookingTime', 'bookingUpdateTime']
         self.booking_related_message_dict = dict()
         for key in booking_related_message_key:
             self.booking_related_message_dict[key] = None
-        self.message_info['bookingRelatedMessage'] = self.booking_related_message_dict
+        #self.message_info['bookingRelatedMessage'] = self.booking_related_message_dict
+        # 註解掉因為現在回傳用不到這個字典
         # 因為booking資訊是在message裡面，先更新message字典再把message字典更新到info大字典
         self.response_msg['messageInfo'] = self.message_info
 
@@ -80,53 +82,76 @@ class chat_room_manager:
             return (self.status, self.errCode, self.errMsg, self.data)
 
     # 調出主對話框全部內容
+    # kwargs: userID, user_type
     def chat_main_content(self, **kwargs):
         # 目前先設定0~100之後這個變數可以給前端傳
         # 希望可以達到假設user滑到頂一次給100之類的效果(前端告訴後端user是第幾次滑到頂)
         #number_of_history_start = 0
         #number_of_history_end = 100
         #chat_messages = Messages.objects.filter(group=room_id).order_by("timestamp")[number_of_history_start:number_of_history_end] 
-        
-        # 首先篩選出所有這個人的聊天室
-        user_type = kwargs['user_type']
-        if user_type == 'teacher':
-            #user_teacher = teacher_profile.objects.filter(username= kwargs['userID'])
-            user_chatrooms_with_user = chatroom_info_user2user.objects.filter(teacher_auth_id=kwargs['userID'])
-        elif user_type == 'student':
-            #user_student = student_profile.objects.filter(username= kwargs['userID'])
-            user_chatrooms_with_user = chatroom_info_user2user.objects.filter(student_auth_id=kwargs['userID'])
-        
-        room_queryset=chatroom_info_user2user.objects.filter(Q(student_auth_id=kwargs['userID'])|Q(teacher_auth_id=kwargs['userID'])).order_by("-date")
-        
-        # 如果有資料的前提..
-        # 包成可以回傳給前端的格式
-        for a_chatroom in room_queryset:
-            chatroomID = a_chatroom.id
+        self.data = list()
+        try:
+            # 首先篩選出所有這個人的聊天室
+            user_type = kwargs['user_type']
             if user_type == 'teacher':
-                chatUserID = a_chatroom.student_auth_id
-                chat_user = student_profile.objects.filter(auth_id = a_chatroom.student_auth_id).first()
-                chatUserType = 'student'
+                #user_teacher = teacher_profile.objects.filter(username= kwargs['userID'])
+                user_chatrooms_with_user = chatroom_info_user2user.objects.filter(teacher_auth_id=kwargs['userID'])
             elif user_type == 'student':
-                chatUserID = a_chatroom.teacher_auth_id
-                chat_user = student_profile.objects.filter(auth_id = a_chatroom.teacher_auth_id).first()
-                chatUserType = 'teacher'
+                #user_student = student_profile.objects.filter(username= kwargs['userID'])
+                user_chatrooms_with_user = chatroom_info_user2user.objects.filter(student_auth_id=kwargs['userID'])
             
-            chatUserPath = chat_user.thumbnail_dir
-            chatUserName = chat_user.nickname
-            ### 紀錄一下最外層目前已有這些資訊 response_value = [chatroomID, chatUserID, chatUserType, chatUserName]
-            # 接下來是 chatUnreadMessageQty
-            # 歷史訊息的id = roomid,且發送者不是 user, 且未讀 = 0
-            chat_history_obj = chat_history_user2user.objects.filter(Q(chatroom_info_user2user_id=chatroomID)&Q(is_read = 0)& ~Q(who_is_sender = kwargs['userID']))
-            chatUnreadMessageQty = len(chat_history_obj)
-            
-            # messageInfo 每一則訊息的資訊
-            # messageType: 訊息類別(0:一般文字, 1:系統訊息, 2:預約方塊)
-            all_messages = chat_history_user2user.objects.filter(chatroom_info_user2user_id=chatroomID).order_by("timestamp")
-            for a_message in all_messages:
-                msg_sender = a_message.who_is_sender
-                if msg_sender == 1:
-                    pass
+            room_queryset= chatroom_info_user2user.objects.filter(Q(student_auth_id=kwargs['userID'])|Q(teacher_auth_id=kwargs['userID'])).order_by("-created_time")
+            print('使用者有這些聊天室')
+            print(room_queryset)
+            # 如果有資料的前提..
+            # 包成可以回傳給前端的格式
+            for a_chatroom in room_queryset:
+                #temp_chat_room_info = dict()
+                chatroomID = a_chatroom.id
+                if user_type == 'teacher':
+                    chatUserID = a_chatroom.student_auth_id
+                    chat_user = student_profile.objects.filter(auth_id = a_chatroom.student_auth_id).first()
+                    chatUserType = 'student'
+                elif user_type == 'student':
+                    chatUserID = a_chatroom.teacher_auth_id
+                    chat_user = student_profile.objects.filter(auth_id = a_chatroom.teacher_auth_id).first()
+                    chatUserType = 'teacher'
                 
+                chatUserPath = chat_user.thumbnail_dir
+                chatUserName = chat_user.nickname
+                ### 紀錄一下最外層目前已有這些資訊 response_value = [chatroomID, chatUserID, chatUserType, chatUserName]
+                # chatUnreadMessageQty 歷史訊息的id = roomid,且發送者不是 user, 且未讀 = 0
+                chat_history_obj = chat_history_user2user.objects.filter(Q(chatroom_info_user2user_id=chatroomID)&Q(is_read = 0)& ~Q(who_is_sender = kwargs['userID']))
+                chatUnreadMessageQty = len(chat_history_obj)
+                update_response_msg = {'chatroomID':chatroomID,'chatUnreadMessageQty':chatUnreadMessageQty,
+                'chatUserID' : chatUserID, 'chatUserType': chatUserType ,
+                            'chatUserName' : chatUserName, 'chatUserPath' : chatUserPath}
+                            #測試看看用下面的selfinfo 訊息是否可以直接更新到self.response 
+                            # 如果不行再加這段 'messageInfo' = message_info} #並且移到最下面
+                self.response_msg.update(update_response_msg)
+
+                # messageInfo 每一則訊息的資訊
+                # messageType: 訊息類別(0:一般文字, 1:系統訊息, 2:預約方塊)
+                all_messages = chat_history_user2user.objects.filter(chatroom_info_user2user_id=chatroomID).order_by("created_time")
+                for a_message in all_messages:
+                    temp_info = dict()
+                    temp_info['senderID'] = a_message.who_is_sender
+                    temp_info['messageType'] = a_message.message_type
+                    temp_info['messegText'] = a_message.message
+                    temp_info['messageCreateTime'] = a_message.created_time
+                    self.message_info.append(temp_info)
+                
+                self.data.append(self.response_msg)
+            return (self.status, self.errCode, self.errMsg, self.data)
+        
+        except Exception as e:
+            print(e)
+            self.status = 'failed'
+            self.errCode = '1'
+            self.errMsg = 'Querying Data Failed.'
+            return (self.status, self.errCode, self.errMsg, self.data)
+
+        
         
     def chat_customer_service_messeage(self):
         pass
