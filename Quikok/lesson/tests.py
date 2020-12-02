15ac3fa1919e7696e75c30bdc358085ceb4355ab
tamio <tamio.chou@gmail.com>
2020-12-02 17:19:29 +0800

    第一次收取JSON response成功

--- Quikok/lesson/tests.py
+++ Quikok/lesson/tests.py
@@ -11,9 +11,16 @@ class Lesson_Related_Functions_Test(TestCase):
         # self.factory = RequestFactory()
         self.client = Client()
         response = self.client.post(path='/api/lesson/beforeSigningUpCreateOrEditLesson/')
-        print(response.context)
-        self.assertIn('success', response.content.decode())
-
+        self.assertEqual(response.status_code, 200)
+        print(str(response.content, encoding='utf8'))
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'status': 'success',
+                'errCode': None,
+                'errMsg': None,
+            }
+        )
 
     '''def test_before_signing_up_create_or_edit_a_lesson_exist(self):
         # 確認有找到這個views function
