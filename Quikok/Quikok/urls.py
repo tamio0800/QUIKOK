bcd9bfed8bd04476706287bbb91597729dc56338
tamio <tamio.chou@gmail.com>
2020-12-02 10:55:13 +0800

    建立blog app，並且測試連線完成!

--- Quikok/Quikok/urls.py
+++ Quikok/Quikok/urls.py
@@ -10,15 +10,16 @@ from django.views.generic import TemplateView  # 新增的
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('django_api.urls')),
     path('api/chat/', include('chatroom.urls')),
     path('chat/', include('chatroom.urls')),
-    path('homepage/', views.homepage, name='home'),
-    path('base_layout/', views.base_layout),
     path('api/account/', include('account.urls')),
     path('api/lesson/', include('lesson.urls')),
+    path('articles/', include('blog.urls')),
+    path('homepage/', views.homepage, name='home'),
+    path('base_layout/', views.base_layout),
     path('test/', views.test_page),
     path('api/getBannerBar/', views.get_banner_bar),
-    path('api/', include('django_api.urls')),
     re_path(r'^(?!.*?user_upload|.*?website_assets|.*?@\S+[.]com).*', TemplateView.as_view(template_name="index.html")),  # 新增的
 
 ]
