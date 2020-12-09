4f8816c5a742feab314caedb54329efb676550ba
tamio <tamio.chou@gmail.com>
2020-12-09 12:42:13 +0800

    在admin註冊部落格的資料庫table

--- Quikok/blog/admin.py
+++ Quikok/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import article_info, author_profile
 
 # Register your models here.
+admin.site.register(article_info)
+admin.site.register(author_profile)
+
