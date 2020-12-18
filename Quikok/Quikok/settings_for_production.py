dab8d2c8d4052cd6c8780fc3dbf42a2bfdfb1100
tamio <tamio.chou@gmail.com>
2020-12-18 17:42:05 +0800

    設定完settings_for_production

---
+++ Quikok/Quikok/settings_for_production.py
@@ -0,0 +1,16 @@
+
+from Quikok.settings import *
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'quikok_db_production',  
+        'USER': 'root',
+        'PASSWORD': '@Annie0800_GaryWx2003_tamiotsiu+#YT#',
+        'HOST': '61.222.157.152',
+        'PORT': '3306',
+    }
+}
+DEBUG = False
+ALLOWED_HOSTS = ['*']
