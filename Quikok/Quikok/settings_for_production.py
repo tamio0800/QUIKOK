1c198d28290d5ec6810608a571d93595897fafd4
tamio <tamio.chou@gmail.com>
2020-12-22 00:29:00 +0800

    修改wsgi.py檔案，並將blog中的大圖片強制調整為1100 x 550的大小

--- Quikok/Quikok/settings_for_production.py
+++ Quikok/Quikok/settings_for_production.py
@@ -1,4 +1,3 @@
-
 from Quikok.settings import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- Quikok/Quikok/wsgi.py
+++ Quikok/Quikok/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings_for_production')
 
 application = get_wsgi_application()
