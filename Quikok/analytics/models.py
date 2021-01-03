27466d45f8ffddd852ce8b0031f520eefd02e178
tamio <tamio.chou@gmail.com>
2021-01-03 22:48:07 +0800

    更新 object access 呈現

--- Quikok/analytics/models.py
+++ Quikok/analytics/models.py
@@ -20,7 +20,7 @@ class object_accessed_info(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.object_name} viewed on {self.timestamp}'
+        return f'{self.object_name} viewed by {self.ip_address} on {str(self.timestamp).split(".")[0]}'
 
     class Meta:
         ordering= ['-timestamp']  # 越新的會被呈現在越上面
