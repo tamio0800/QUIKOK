2c37a73d97fdde92220457e5540e79c1a0be744d
tamiotsiu <tamio.chou@gmail.com>
2021-01-03 22:51:34 +0800

    更改 analytics 呈現的格式

--- Quikok/analytics/models.py
+++ Quikok/analytics/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.dispatch import receiver
 from analytics.signals import object_accessed_signal
+from datetime import timedelta
 
 # User = settings.AUTH_USER_MODEL
 
@@ -20,7 +21,7 @@ class object_accessed_info(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.object_name} viewed by {self.ip_address} on {str(self.timestamp).split(".")[0]}'
+        return f'{self.object_name} viewed by {self.ip_address} on {str(self.timestamp + timedelta(hours=8)).split(".")[0]}'
 
     class Meta:
         ordering= ['-timestamp']  # 越新的會被呈現在越上面
