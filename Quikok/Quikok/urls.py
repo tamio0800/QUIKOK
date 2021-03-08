9c4354ec9406e3278035f495cbbcfc4fae467b06
tamiotsiu <tamio.chou@gmail.com>
2021-03-08 16:19:56 +0800

    整合

--- Quikok/Quikok/urls.py
+++ Quikok/Quikok/urls.py
@@ -17,11 +17,10 @@ urlpatterns = [
     path('api/lesson/', include('lesson.urls')),
     path('api/account_finance/', include('account_finance.urls')),
     path('api/getBannerBar/', views.get_banner_bar),
-    path('ws/chat/', include('chatroom.urls')),
+    # path('ws/chat/', include('chatroom.urls')),
     path('articles/', include('blog.urls')),
     path('edony_linebot/', include('line_function.urls')),
     path('analytics/', include('analytics.urls')),
-    
     re_path(r'^(?!.*?user_upload|.*?website_assets|.*?@\S+[.]com).*', TemplateView.as_view(template_name="index.html")),  # 新增的
 ]
 
