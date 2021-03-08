bb70ec4e5a06e359ff3d214f4aaf0cb540cf7ddf
tamio <tamio.chou@gmail.com>
2021-03-08 18:42:47 +0800

    將wsgi 與 asgi設定為有測試版跟產品

--- Quikok/Quikok/asgi.py
+++ Quikok/Quikok/asgi.py
@@ -6,11 +6,14 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
-
+from django.conf import settings
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings')
+if settings.DEV_MODE == False:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings_for_production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings')
 
 application = get_asgi_application()
