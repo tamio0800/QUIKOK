9e993195f67007797f301afa01d145734ee520cc
tamio <tamio.chou@gmail.com>
2021-03-08 22:42:29 +0800

    創建8999的一般api與6379的ws api, 聊天室runserver可跑

--- Quikok/Quikok/wsgi.py
+++ Quikok/Quikok/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings_for_production')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quikok.settings')
+# _for_production
 application = get_wsgi_application()
